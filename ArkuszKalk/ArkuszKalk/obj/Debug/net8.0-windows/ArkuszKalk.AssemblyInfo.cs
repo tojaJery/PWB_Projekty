bb40a0011e1173adaa2b7a7858edeabab4ebd749
Jeremy Bełezina <63468130+tojaJery@users.noreply.github.com>
2025-05-30 14:11:11 +0200

    Gotowe zliczanie Avalonia

--- ArkuszKalk/ArkuszKalk/obj/Debug/net8.0-windows/ArkuszKalk.AssemblyInfo.cs
+++ ArkuszKalk/ArkuszKalk/obj/Debug/net8.0-windows/ArkuszKalk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArkuszKalk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a31f8ef8d1aed9559d1e9c9d6857a2193f48d56d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b7cea5bf72bf358baf888a2d46e3c78841b430")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArkuszKalk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArkuszKalk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
